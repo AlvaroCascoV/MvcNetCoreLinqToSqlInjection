fdd5ba5826ed063526fbf69cd1dbdb9d0d969fec
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-02-05 11:48:43 +0100

    Add project files.

---
+++ MvcNetCoreLinqToSqlInjection/Models/Deportivo.cs
@@ -0,0 +1,38 @@
+﻿namespace MvcNetCoreLinqToSqlInjection.Models
+{
+    public class Deportivo: ICoche
+    {
+        public Deportivo() 
+        {
+            this.Marca = "Porsche";
+            this.Modelo = "911 GT3RS";
+            this.Imagen = "gt3rs.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 296;
+        }
+
+        public string Marca { get ; set ; }
+        public string Modelo { get ; set ; }
+        public string Imagen { get ; set ; }
+        public int Velocidad { get ; set ; }
+        public int VelocidadMaxima { get ; set ; }
+
+        public void Acelerar()
+        {
+            this.Velocidad += 45;
+            if (this.Velocidad >= this.VelocidadMaxima)
+            {
+                this.Velocidad = this.VelocidadMaxima;
+            }
+        }
+
+        public void Frenar()
+        {
+            this.Velocidad -= 25;
+            if (this.Velocidad < 0)
+            {
+                this.Velocidad = 0;
+            }
+        }
+    }
+}
---
+++ MvcNetCoreLinqToSqlInjection/Program.cs
@@ -0,0 +1,46 @@
+using MvcNetCoreLinqToSqlInjection.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//otra forma de añadir un coche
+Coche car = new Coche();
+car.Marca = "FORD";
+car.Modelo = "MUSTANG DARKHORSE";
+car.Imagen = "darkhorse.jpg";
+car.Velocidad = 0;
+car.VelocidadMaxima = 250;
+builder.Services.AddSingleton<ICoche, Coche>(x => car);
+
+//Resolvemos el Servicio Coche para la inyeccion
+//builder.Services.AddTransient<Coche>(); los transients devuelven objeto nuevo al cargar, no sirve para forms pq lo pone a 0
+//builder.Services.AddSingleton<Coche>();
+//builder.Services.AddSingleton<Deportivo>();
+//builder.Services.AddSingleton<ICoche, Deportivo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
